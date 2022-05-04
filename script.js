7e2dc8dbcfb1ff5438359ffad554881332c354c9
Saniddhya Nigam <saniddhya_nigam@epam.com>
2022-05-04 14:58:00 +0530

    Added JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+Loader=document.getElementById("loader")
+QuoteContaner=document.getElementById("QuoteC")
+let quotes =[]
+async function getQuote(){
+    l()
+    const apiurl ="https://type.fit/api/quotes"
+    try {
+        const response = await fetch(apiurl)
+        quotes= await response  .json()
+        console.log(quotes)
+        
+        myFunction()
+    } catch (error) {
+        
+    }
+    c()
+    
+}
+getQuote()
 
+function myFunction(){
+    let randquote = quotes[Math.floor(Math.random() * quotes.length)]
+    console.log(randquote.text)
+    document.getElementById("Quote").textContent= randquote.text
+    document.getElementById("A").textContent = randquote.author
+
+
+}
+
+function l(){
+    Loader.hidden=false
+    QuoteContaner.hidden=true
+}
+function c(){
+    Loader.hidden=true
+    QuoteContaner.hidden=false
+}
